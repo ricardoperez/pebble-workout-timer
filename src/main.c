ee4785c2fb33601d1fb2d32818ff4b47e963e781
Ricardo Perez <riperezzz@gmail.com>
2016-02-20 23:50:44 +0000

    functional workout set timer.

--- src/main.c
+++ src/main.c
@@ -5,58 +5,71 @@
 Window *window;
 TextLayer *text_layer;
 int *counter;
-int teipo = 30;
-int repeat = 3;
-int wait = 2; 
+
+static void timer(void *context);
+
+int tempo = 15;
+int repeat = 2;
+int wait = 3; 
+int original_tempo;
+int original_wait;
 
 void update_timer_text(void){
   static char s_buffer[4];
-  snprintf(s_buffer, sizeof(s_buffer), "%d", teipo);
+  snprintf(s_buffer, sizeof(s_buffer), "%d", tempo);
 	text_layer_set_text(text_layer, s_buffer);
 }
 
-void repeat_timer(void){
-  
+void wait_for(void *context){
+  wait--;
+	text_layer_set_text(text_layer, "WAIT MATE");
+  if (wait > 0){
+    app_timer_register(1000, wait_for, NULL);
+  }else{
+    repeat--;
+    tempo = original_tempo;
+    wait = original_wait;
+    vibes_short_pulse();
+    app_timer_register(1000, timer, NULL);
+  }
 }
 
-void wait_for(void){
-  
+void cabo(void){
+  vibes_double_pulse();
+  text_layer_set_text(text_layer, "DONE!!");
 }
 
-void timer(void *context) {
-  teipo--;  
+static void timer(void *context) {
+  tempo--;  
   update_timer_text();
-  if (teipo > 0 ){
+  if (tempo > 0 ){
     app_timer_register(1000, timer, NULL);
   }else{
     vibes_long_pulse();
+    (repeat > 0) ? wait_for(NULL) : cabo();
   }
 }
 
 void handle_init(void) {
+  original_tempo = tempo;
+  original_wait = wait;
 	// Create a window and text layer
 	window = window_create();
-	
   text_layer = text_layer_create(GRect(0, 30, 144, 154));
 	update_timer_text();
 	// Set the text, font, and text alignment
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  
   app_timer_register(1000, timer, NULL);
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
 	// Push the window
 	window_stack_push(window, true);
-	
-	// App Logging!
 }
 
 void handle_deinit(void) {
 	// Destroy the text layer
 	text_layer_destroy(text_layer);
-	
 	// Destroy the window
 	window_destroy(window);
 }
